d16350362a1cd35a1b67cb2b94bb25993ccabc4e
Tura-lab <55505646+Tura-lab@users.noreply.github.com>
2021-02-04 00:21:56 -0800

    starting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { input: "" };
+  }
+  render() {
+    return <p>Good</p>;
+  }
 }
+export default App;
